626f689df286adc2cd5edc41cd64d81b9075c32f
shunhe912 <shunheyuan912@gmail.com>
2017-06-17 01:15:02 +0800

    init project with ionic 3.4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,59 +1,44 @@
 import { Component, ViewChild } from '@angular/core';
-import { Platform, MenuController, Nav, App } from 'ionic-angular';
-import { StatusBar, Splashscreen } from 'ionic-native';
+import { Nav, Platform } from 'ionic-angular';
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
 
-import { TabsNavigationPage } from '../pages/tabs-navigation/tabs-navigation';
-import { WalkthroughPage } from '../pages/walkthrough/walkthrough';
-import { SettingsPage } from '../pages/settings/settings';
+import { HomePage } from '../pages/home/home';
+import { ListPage } from '../pages/list/list';
 
 @Component({
-  selector: 'app-root',
   templateUrl: 'app.html'
 })
 export class MyApp {
-
   @ViewChild(Nav) nav: Nav;
 
-  // make WalkthroughPage the root (or first) page
-  rootPage: any = WalkthroughPage;
-  // rootPage: any = TabsNavigationPage;
+  rootPage: any = HomePage;
 
-  pages: Array<{title: string, icon: string, component: any}>;
-  pushPages: Array<{title: string, icon: string, component: any}>;
+  pages: Array<{title: string, component: any}>;
 
-  constructor(
-    platform: Platform,
-    public menu: MenuController,
-    public app: App
-  ) {
-    platform.ready().then(() => {
+  constructor(public platform: Platform, public statusBar: StatusBar, public splashScreen: SplashScreen) {
+    this.initializeApp();
+
+    // used for an example of ngFor and navigation
+    this.pages = [
+      { title: 'Home', component: HomePage },
+      { title: 'List', component: ListPage }
+    ];
+
+  }
+
+  initializeApp() {
+    this.platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
-      Splashscreen.hide();
-      StatusBar.styleDefault();
+      this.statusBar.styleDefault();
+      this.splashScreen.hide();
     });
-
-    this.pages = [
-      { title: 'Home', icon: 'home', component: TabsNavigationPage }
-    ];
-
-    this.pushPages = [
-      { title: 'Settings', icon: 'settings', component: SettingsPage }
-    ];
   }
 
   openPage(page) {
-    // close the menu when clicking a link from the menu
-    this.menu.close();
-    // navigate to the new page if it is not the current page
-
+    // Reset the content nav to have just this page
+    // we wouldn't want the back button to show in this scenario
     this.nav.setRoot(page.component);
-  }
-
-  pushPage(page) {
-    // close the menu when clicking a link from the menu
-    this.menu.close();
-    // rootNav is now deprecated (since beta 11) (https://forum.ionicframework.com/t/cant-access-rootnav-after-upgrade-to-beta-11/59889)
-    this.app.getRootNav().push(page.component);
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,58 +1,34 @@
-import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
-import { IonicApp, IonicModule, Platform } from 'ionic-angular';
-
-import { Storage } from '@ionic/storage';
+import { BrowserModule } from '@angular/platform-browser';
+import { ErrorHandler, NgModule } from '@angular/core';
+import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 
 import { MyApp } from './app.component';
+import { HomePage } from '../pages/home/home';
+import { ListPage } from '../pages/list/list';
 
-import { LoginPage } from '../pages/login/login';
-import { ProfilePage } from '../pages/profile/profile';
-import { TabsNavigationPage } from '../pages/tabs-navigation/tabs-navigation';
-import { WalkthroughPage } from '../pages/walkthrough/walkthrough';
-import { SettingsPage } from '../pages/settings/settings';
-import { SignupPage } from '../pages/signup/signup';
-import { ForgotPasswordPage } from '../pages/forgot-password/forgot-password';
-import { TermsOfServicePage } from '../pages/terms-of-service/terms-of-service';
-import { PrivacyPolicyPage } from '../pages/privacy-policy/privacy-policy';
-import { ProfileService } from '../pages/profile/profile.service';
-
-import { PreloadImage } from '../components/preload-image/preload-image';
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
 
 @NgModule({
   declarations: [
     MyApp,
-    LoginPage,
-    ProfilePage,
-    TabsNavigationPage,
-    WalkthroughPage,
-    SettingsPage,
-    SignupPage,
-    ForgotPasswordPage,
-    TermsOfServicePage,
-    PrivacyPolicyPage,
-
-    PreloadImage
+    HomePage,
+    ListPage
   ],
   imports: [
-    IonicModule.forRoot(MyApp)
+    BrowserModule,
+    IonicModule.forRoot(MyApp),
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    LoginPage,
-    ProfilePage,
-    TabsNavigationPage,
-    WalkthroughPage,
-    SettingsPage,
-    ForgotPasswordPage,
-    SignupPage,
-    TermsOfServicePage,
-    PrivacyPolicyPage
+    HomePage,
+    ListPage
   ],
   providers: [
-    ProfileService,
-    Storage
-  ],
-  schemas: [ CUSTOM_ELEMENTS_SCHEMA ]
+    StatusBar,
+    SplashScreen,
+    {provide: ErrorHandler, useClass: IonicErrorHandler}
+  ]
 })
 export class AppModule {}
